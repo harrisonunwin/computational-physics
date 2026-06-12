4372c76946489260d3cce16e55b8de498f8c553f
harrisonu777 <harrison.unwin@icloud.com>
2026-06-12 13:53:44 +0100

    commit 3

--- test.py
+++ test.py
@@ -1,2 +1,26 @@
-x = 1.2e2
-print(x)
+x,y=1,3
+print(f"The value of x is {x} and the value of y is {y}")
+
+#Seperator, inputs where the commas are
+w = 3.14159
+z = 1+2j
+print("We can separate variables using sep:",w,z,sep="___")
+
+#modulo operation
+u = 26%3
+print("The remainder of 26/3 is",u)
+
+#Can update variables
+x = x + 1
+print("The updated value of x is",x)
+
+#Python modifiers
+"x += 1"
+"x -= 4"
+"x *= 2.5"
+"x /= 4"
+"x //= 3.2"
+
+#We can swap variables
+x,y = y,x
+print("Now we have swapped the value of x and y, so x =",x,"and y =",y)
