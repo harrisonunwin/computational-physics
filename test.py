7506a8e5cb8391f47322528b72eaf678ee929704
harrisonu777 <harrison.unwin@icloud.com>
2026-07-28 19:12:18 +0100

    semi-empirical mass formula

--- test.py
+++ test.py
@@ -14,4 +14,28 @@ for m in [1,2,3]:
         invlambda = R*(1/m**2 - 1/n**2)
         print("   ",1/invlambda,"nm")
 
+#Semi-empirical mass formula
+a1 = 15.8 #units of millions of eV
+a2 = 18.3
+a3 = 0.714
+a4 = 23.2
+
+A = float(input("Enter the mass number:"))
+Z = float(input("Enter the atomic number:"))
+
+if A%2 != 0:
+    a5 = 0.0
+
+elif Z%2 == 0:
+    a5 = 12.0
+
+else:
+    a5 = -12.0
+
+B = a1*A - a2*A**(2/3) - (a3*Z**2)/A**(1/3) -(a4*(A-2*Z)**2)/A + a5/A**(1/2)
+
+print("The binding energy is",B,"MeV")
+
+
+
 
