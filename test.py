478e999751d0ad09512e0883a81a279dd2ab2011
harrisonu777 <harrison.unwin@icloud.com>
2026-07-28 18:54:29 +0100

    spectral lines using for loop

--- test.py
+++ test.py
@@ -1,46 +1,17 @@
-x,y=1,3
-print(f"The value of x is {x} and the value of y is {y}")
+#For loops
 
-#Seperator, inputs where the commas are
-w = 3.14159
-z = 1+2j
-print("We can separate variables using sep:",w,z,sep="___")
+s=0
+for n in range(1,101):
+    s += 1/n
+print(s)
 
-#modulo operation
-u = 26%3
-print("The remainder of 26/3 is",u)
-
-#Can update variables
-x = x + 1
-print("The updated value of x is",x)
-
-#Python modifiers
-"x += 1"
-"x -= 4"
-"x *= 2.5"
-"x /= 4"
-"x //= 3.2"
-
-#We can swap variables
-x,y = y,x
-print("Now we have swapped the value of x and y, so x =",x,"and y =",y)
-
-#Ball drop
-from math import sqrt, pi
-height = float(input("Please enter your height in meters:"))
-g = 9.81
-time = sqrt(2*height/g)
-print(f"The time it takes for the ball to reach the ground is: {time}")
-
-
-#Altitude for orbit with time period T
-
-G = 6.67e-11
-M = 5.97e24
-R = 6.371e6
-T = float(input("Please enter the time period your orbit in seconds:"))
-h = (G*M*T**2/(4*pi**2))**(1/3) - R
-print(f"The height of your obit is: {h}")
-
+#Emission lines
+R = 1.097e-2
+for m in [1,2,3]:
+    print("Series for m = ",m)
+    for k in range(1,6):
+        n = m + k
+        invlambda = R*(1/m**2 - 1/n**2)
+        print("   ",1/invlambda,"nm")
 
 
