7128f682891d88c391595a1da74019ed7c6ea638
harrisonu777 <harrison.unwin@icloud.com>
2026-08-12 11:44:48 +0100

    simulating brownian motion, kept particle confined to lattice LxL.

--- random-numbers/brownian_motion.py
+++ random-numbers/brownian_motion.py
@@ -1,10 +1,33 @@
 #Create LxL lattice
 from matplotlib import pyplot as plt
+from random import random
 
 L = 101
+steps = 1000000
 j_points = range(L)
 i_points = range(L)
 
 i,j = 50,50    #start points in centre of grid
 
 
+for s in range(steps):
+
+    if random() < 0.5:
+        if random() < 0.5:
+            if i < L-1:
+                i += 1
+        else:
+            if i > 0:
+                i -= 1
+
+    else:
+        if random() < 0.5:
+            if j < L-1:
+                j += 1
+        else:
+            if j > 0:
+                j -= 1
+
+
+
+print(i,j)
