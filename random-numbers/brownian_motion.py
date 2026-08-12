457412f43947a0a0a24d1a51b788a7f91eb663d3
harrisonu777 <harrison.unwin@icloud.com>
2026-08-12 12:40:53 +0100

    added animation

--- random-numbers/brownian_motion.py
+++ random-numbers/brownian_motion.py
@@ -1,13 +1,12 @@
-#Create LxL lattice
+
 from matplotlib import pyplot as plt
 from random import random
+import matplotlib.animation as animation
 
 L = 101
 steps = 1000000
-j_points = range(L)
-i_points = range(L)
-
-i,j = 50,50    #start points in centre of grid
+i = j = L//2        #start points in centre of grid
+i_steps, j_steps = [i], [j]
 
 
 for s in range(steps):
@@ -28,6 +27,28 @@ for s in range(steps):
             if j > 0:
                 j -= 1
 
+    i_steps.append(i)
+    j_steps.append(j)
 
+print("The particle is situated at point",i,j)
 
-print(i,j)
+#set up grid
+fig, ax = plt.subplots()
+ax.set_xlim(0, L - 1)
+ax.set_ylim(0, L - 1)
+ax.set_aspect('equal')
+ax.grid(True, linestyle='--', alpha=0.4)
+
+trail, = ax.plot([], [], linewidth=0.5)       # the path so far
+point, = ax.plot([], [], 'ro')
+
+def update(frame):
+    trail.set_data(i_steps[:frame+1], j_steps[:frame+1])
+    point.set_data([i_steps[frame]], [j_steps[frame]])
+    return trail, point
+
+ani = animation.FuncAnimation(
+    fig, update, frames=len(i_steps), interval=20, blit=True
+)
+
+plt.show()
