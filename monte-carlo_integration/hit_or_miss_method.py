1cd748d8c96b79baabebf95d4a05cbd285742e40
harrisonu777 <harrison.unwin@icloud.com>
2026-08-14 17:38:55 +0100

    updated to calculate the error on the integral

--- monte-carlo_integration/hit_or_miss_method.py
+++ monte-carlo_integration/hit_or_miss_method.py
@@ -1,21 +1,33 @@
 from random import random
-from math import sin
+from math import sin, sqrt
 
 def f(x):
     s = sin(1/(x*(2-x)))**2
     return s
 
-def main():
-    N = 10000
+N = 10000
+area = 2
+
+def monte_carlo():
     count = 0
     for i in range(N):
-        x = 2*random()
+        x = area*random()
         y = random()
         if y < f(x):
             count += 1
 
-    I = 2*count/N
-    print("The value of the integrl is",I)
+    I = area * count / N
+    return I
+
+def error(I):
+    std = sqrt(I * (area - I) / N)
+    return std
+
+def main():
+    I = monte_carlo()
+    std = error(I)
+    print("Value of integral is",I)
+    print("Error is",std)
 main()
 
 
